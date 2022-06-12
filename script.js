e0796fce86bf1232fd6ef1b7f23451fc0b5fd013
Sivapptvl <106486254+Sivapptvl@users.noreply.github.com>
2022-06-12 16:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ var audio = document.getElementById("audio");
 var submit = document.getElementById("submit");
 
 
-
+// Placeholder to coordinate box
 coords.placeholder = "Paste      <LATITUDE>,<LONGITUDE>      here";
 
 
-
+// Range of radius 
 radius.min = 200;
 radius.max = 2000;
 radius.value = 1000;
@@ -24,17 +24,19 @@ radius.addEventListener("touchmove", ()=>{
 });
 
 
-
+// Vibration controls
 vibration.textContent = "OFF";
 var vibrate = false;
 
 vibration.addEventListener('pointerdown', ()=>{
     if(vibrate){
+        // if ON, => OFF
         vibrate = false;
         vibration.style.color = "#ff0a00";
         vibration.textContent = "OFF";
     }
     else{
+        // if OFF, => ON
         vibrate = true;
         vibration.style.color = "#26ff00";
         vibration.textContent = "ON";return
@@ -42,6 +44,6 @@ vibration.addEventListener('pointerdown', ()=>{
 });
 
 
-
+// audio link
 audio.value = "";
 audio.placeholder = "Paste link";
